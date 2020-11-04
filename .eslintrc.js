2b4b841e552988cd67079abc36c3d8d0bcd6fe8e
Mohammad <mohammadidehgamwala5@gmail.com>
2020-11-04 13:09:21 +0530

    changes in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,33 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: ['plugin:react/recommended', 'airbnb'],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    plugins: ['react'],
-    rules: {},
-}
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'max-len': ['error', { code: 100 }],
+    'prefer-promise-reject-errors': ['off'],
+    'react/jsx-filename-extension': ['off'],
+    'react/prop-types': ['warn'],
+    'no-return-assign': ['off'],
+    'react/jsx-closing-bracket-location': 0,
+    'import/no-extraneous-dependencies': 0,
+    'react/jsx-props-no-spreading': 0,
+    'import/no-cycle': 0,
+    'object-curly-newline': 0,
+    'no-unused-expressions': 0,
+    'react/jsx-wrap-multilines': 0,
+    'react/forbid-prop-types': 0,
+    indent: 0,
+    'implicit-arrow-linebreak ': 0,
+    'operator-linebreak': 0,
+  },
+};
