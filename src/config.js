21e24f70ff1d4e0a12574ce7ce8c53749696553f
Mohammad <mohammadidehgamwala5@gmail.com>
2020-11-07 23:07:24 +0530

    Changes in public folder

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyAcO3tvMYbeAmFtsbFFohxrTZ_CD0xodP4',
+  authDomain: 'cakes-fc126.firebaseapp.com',
+  databaseURL: 'https://cakes-fc126.firebaseio.com',
+  projectId: 'cakes-fc126',
+  storageBucket: 'cakes-fc126.appspot.com',
+  messagingSenderId: '12090665833',
+  appId: '1:12090665833:web:d4e03de5a95122a341abf8',
+};
